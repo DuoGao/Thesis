de3432278ae80f8fe5f4ad266afff05885652213
DuoGao <duo.gao@studio.unibo.it>
2017-03-06 15:42:15 +0100

    Update commons.hpp

--- duo_slam/include/slamdunk_extension/cuda/commons.hpp
+++ duo_slam/include/slamdunk_extension/cuda/commons.hpp
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   commons.hpp
- * Author: daniele
- *
- * Created on July 15, 2016, 12:53 PM
- */
 
 #ifndef COMMONS_HPP
 #define COMMONS_HPP
