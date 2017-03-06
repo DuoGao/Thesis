402a70ca60df8c057b0b81242b7f08a56faba8fa
DuoGao <duo.gao@studio.unibo.it>
2017-03-06 15:41:43 +0100

    Update CudaTsdf.h

--- duo_slam/include/slamdunk_extension/cuda/CudaTsdf.h
+++ duo_slam/include/slamdunk_extension/cuda/CudaTsdf.h
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   CudaTsdf.h
- * Author: daniele
- *
- * Created on July 15, 2016, 12:39 PM
- */
 
 #ifndef CUDATSDF_H
 #define CUDATSDF_H
