cc8011bed4a83437408fc6b951bc6bf758e4074e
DuoGao <duo.gao@studio.unibo.it>
2017-03-06 15:45:49 +0100

    Update SlamDunkScene.cpp

--- duo_slam/src/slamdunk_extension/SlamDunkScene.cpp
+++ duo_slam/src/slamdunk_extension/SlamDunkScene.cpp
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   SlamDunkScene.cpp
- * Author: Duo Gao
- * 
- * Created on June 16, 2016, 4:51 PM
- */
 
 #include "SlamDunkScene.h"
 
